4f4ee6273563b58ff8ad06aa10397782d9c79910
Kyle Dewey <kyledewey@cs.ucsb.edu>
2017-11-08 12:00:41 -0800

    added support for delimiters and multi-line files

--- src/main/scala/zip_collate/main.scala
+++ src/main/scala/zip_collate/main.scala
@@ -1,6 +1,33 @@
 package zip_collate
 
 object Main {
+  def readUntilDelim(delim: String, readLine: () => String): Option[String] = {
+    val buffer = scala.collection.mutable.Buffer[String]()
+    var line = readLine()
+    while ((line ne null) && line != delim) {
+      buffer += line + "\n"
+      line = readLine()
+    }
+
+    if (buffer.isEmpty) {
+      None
+    } else {
+      Some(buffer.mkString)
+    }
+  }
+
+  def readWithDelim(delim: Option[String], readLine: () => String): Option[String] = {
+    delim match {
+      case None => Option(readLine())
+      case Some(d) => readUntilDelim(d, readLine)
+    }
+  }
+
+  def readWithDelim(delim: Option[String]): Option[String] = {
+    import scala.io.StdIn.readLine
+    readWithDelim(delim, () => readLine())
+  }
+
   def runZipper(args: Args) {
     val zipper = new Zipper(args)
     Runtime.getRuntime.addShutdownHook(
@@ -10,12 +37,17 @@ object Main {
 	    zipper.close()
 	  }
 	}))
-    import scala.io.StdIn.readLine
-    var line: String = readLine()
-    while (line ne null) {
-      zipper.addFile(line)
-      line = readLine()
+
+    def readOne(): Option[String] = {
+      readWithDelim(args.delim)
     }
+
+    var item: Option[String] = readOne()
+    while (item.isDefined) {
+      zipper.addFile(item.get)
+      item = readOne()
+    }
+
     zipper.close()
   } // runZipper
     
